18f53e8591578a88192395dc2d3763b1f68e6435
Fredi Kats <inredikawb@gmail.com>
2021-01-30 16:44:10 +0300

    chore: merge code from old repo

---
+++ Kysect.GithubActivityAnalyzer/GithubActivityProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Kysect.GithubActivityAnalyzer.Models;
+using Newtonsoft.Json;
+
+namespace Kysect.GithubActivityAnalyzer
+{
+    public class GithubActivityProvider
+    {
+        private const string Url = "https://github-contributions.now.sh/api/v1/";
+
+        private readonly HttpClient _client;
+
+        public GithubActivityProvider()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<ActivityInfo> GetActivityInfo(string username)
+        {
+            string response = await _client.GetStringAsync(Url + username);
+            return JsonConvert.DeserializeObject<ActivityInfo>(response);
+        }
+    }
+}
---
+++ Kysect.GithubActivityAnalyzer/Models/ContributionsInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Kysect.GithubActivityAnalyzer.Models
+{
+    public class ContributionsInfo
+    {
+        public string Date { get; set; }
+        public int Count { get; set; }
+
+        public ContributionsInfo(string date, int count)
+        {
+            Date = date;
+            Count = count;
+        }
+
+        public ContributionsInfo()
+        {
+        }
+    }
+}
