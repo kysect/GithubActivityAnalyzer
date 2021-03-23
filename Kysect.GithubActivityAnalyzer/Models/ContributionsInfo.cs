63361951de2033be93d5638c0badf76567baea02
TomGnill <s.drun@yandex.ru>
2021-03-23 03:04:32 +0300

    GetActivityForPeriod+

--- Kysect.GithubActivityAnalyzer/Models/ContributionsInfo.cs
+++ Kysect.GithubActivityAnalyzer/Models/ContributionsInfo.cs
@@ -1,14 +1,19 @@
-﻿namespace Kysect.GithubActivityAnalyzer.Models
+﻿using System;
+using System.Globalization;
+
+namespace Kysect.GithubActivityAnalyzer.Models
 {
     public class ContributionsInfo
     {
         public string Date { get; set; }
+        public DateTime date;
         public int Count { get; set; }
 
         public ContributionsInfo(string date, int count)
         {
             Date = date;
             Count = count;
+            this.date = Convert.ToDateTime(date, CultureInfo.InvariantCulture);
         }
 
         public ContributionsInfo()
