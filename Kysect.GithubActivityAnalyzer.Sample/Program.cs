87c5b41cd305cfd5020fea73b946df9b8c981a5f
TomGnill <s.drun@yandex.ru>
2021-03-23 14:17:10 +0300

    some refact

--- Kysect.GithubActivityAnalyzer.Sample/Program.cs
+++ Kysect.GithubActivityAnalyzer.Sample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Kysect.GithubActivityAnalyzer.Sample
 {
@@ -7,13 +6,6 @@ namespace Kysect.GithubActivityAnalyzer.Sample
     {
         static void Main(string[] args)
         {
-            /*
-            GithubActivityProvider activityProvider = new GithubActivityProvider();
-            var contributions = activityProvider.GetActivityInfo("TomGnill").Result;
-            DateTime from = new DateTime(2020, 09, 03);
-            DateTime to = new DateTime(2020, 09, 05);
-            Console.WriteLine(contributions.GetActivityForPeriod(from, to));//Сошлось :)
-            */
         }
     }
 }
