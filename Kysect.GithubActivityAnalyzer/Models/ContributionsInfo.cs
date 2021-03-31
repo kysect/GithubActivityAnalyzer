3cb881f679d1a810a5d6925cfe9966d0529f304c
TomGnill <s.drun@yandex.ru>
2021-03-31 13:34:09 +0300

    delete DateAsString

--- Kysect.GithubActivityAnalyzer/Models/ContributionsInfo.cs
+++ Kysect.GithubActivityAnalyzer/Models/ContributionsInfo.cs
@@ -6,14 +6,12 @@ namespace Kysect.GithubActivityAnalyzer.Models
 {
     public class ContributionsInfo
     { 
-        [JsonProperty("dateAsString")]
-        public string DateAsString { get; set; }
+        [JsonProperty("date")]
         public DateTime Date { get; set; }
         public int Count { get; set; }
 
         public ContributionsInfo(string dateAsString, int count)
         {
-            DateAsString = dateAsString;
             Count = count;
             this.Date = DateTime.Parse(dateAsString, CultureInfo.InvariantCulture);
         }
