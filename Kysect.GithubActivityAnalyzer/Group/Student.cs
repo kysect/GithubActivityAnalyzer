fcd246bfa2d2cf61ee5a8d6cf6766c1c69e5a3f1
TomGnill <s.drun@yandex.ru>
2021-03-31 17:28:03 +0300

    Analysis methos for groups and student.

---
+++ Kysect.GithubActivityAnalyzer/Group/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kysect.GithubActivityAnalyzer.Models;
+
+namespace Kysect.GithubActivityAnalyzer.Group
+{
+    public class Student
+    {
+        public string Username { get; set; }
+        public ActivityInfo ActivityInfo { get; set; }
+
+        public int TotalContributions
+        {
+            get => ActivityInfo.Total;
+
+            set => TotalContributions = ActivityInfo.Total;
+        }
+
+        public Student(string username)
+        {
+            Username = username;
+            GithubActivityProvider newProvider = new GithubActivityProvider();
+            ActivityInfo = newProvider.GetActivityInfo(username).Result;
+        }
+
+        public int GetActivityForPeriod(DateTime from, DateTime to)
+        {
+            return ActivityInfo.GetActivityForPeriod(from, to);
+        }
+
+        public int GetAverageMonthActivity()
+        {
+            return Convert.ToInt32(ActivityInfo.PerMonthActivity().Average(c => c.Count));
+        }
+    }
+}
---
+++ Kysect.GithubActivityAnalyzer/Group/StudyGroup.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Kysect.GithubActivityAnalyzer.Group
+{
+   public class StudyGroup
+   {
+        public string GroupName { get; set; }
+        public List<Student> Students { get; set; }
+
+        public int TotalContributions
+        {
+            get => (from student in Students select student.TotalContributions).ToArray().Sum();
+
+            private set => TotalContributions = (from student in Students select student.TotalContributions).ToArray().Sum();
+        }
+
+        public StudyGroup(string groupName)
+        {
+            GroupName = groupName;
+            Students = new List<Student>();
+        }
+
+        public StudyGroup(string groupName, List<Student> students)
+        {
+            GroupName = groupName;
+            Students = students;
+        }
+
+        public void AddStudents(params string[] username)
+        {
+            for (int i = 0; i < username.Length; i++)
+            {
+                Students.Add(new Student(username[i]));
+            }
+        }
+
+        public int GetMinValue()
+        {
+           return (from student in Students select student.TotalContributions).ToList().Min();
+        }
+        public int GetMaxValue()
+        {
+           return (from student in Students select student.TotalContributions).ToList().Max();
+        }
+
+        public Student GetMinValueStudent()
+        {
+            return Students.FirstOrDefault(s => s.TotalContributions == GetMinValue());
+        }
+        public Student GetMaxValueStudent()
+        {
+            return Students.FirstOrDefault(s => s.TotalContributions == GetMaxValue());
+        }
+        public int GetAverageValue()
+        {
+            return Convert.ToInt32((from student in Students select student.TotalContributions).ToList().Average()) ;
+        }
+
+        public Dictionary<string, int> GetShortInfo()
+        {
+            Dictionary<string, int> UsersContributions = new Dictionary<string, int>();
+
+            foreach (var student in Students)
+            {
+                UsersContributions.Add(student.Username, student.TotalContributions);
+            }
+
+            return UsersContributions;
+        }
+
+        public int GetActivityForPeriod(DateTime from, DateTime to)
+        { 
+            return Students.Select(student => student.ActivityInfo.GetActivityForPeriod(@from, to)).ToList().Sum();
+        }
+
+        public int GetAverageMonthActivity()
+        {
+            List<int> averegeInts = Students.Select(student => Convert.ToInt32(student.ActivityInfo.PerMonthActivity().Average(c => c.Count))).ToList();
+            return Convert.ToInt32(averegeInts.Average());
+        }
+
+   }
+}
