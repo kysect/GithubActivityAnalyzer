4c284fc6ec7547b68bff92ec28493a86e0ec1173
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-04-03 13:07:20 +0300

    Add db entity, context, repository.

---
+++ Kysect.GithubActivityAnalyzer/DLL/Repositories/ActivityContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kysect.GithubActivityAnalyzer.DLL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kysect.GithubActivityAnalyzer.DLL.Repositories
+{
+    public class ActivityContext : DbContext
+    {
+        public DbSet<UserCash> UserCash { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=D:\\LABS\\Projects\\ActivityParser\\myDB.db");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserCash>()
+                .HasKey(b => b.Username)
+                .HasName("PrimaryKey_Username");
+        }
+    }
+}
