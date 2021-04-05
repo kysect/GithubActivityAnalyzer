409e8fa027a5111aad80eba37ae2ad2ef9051df8
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-04-05 11:31:10 +0300

    Fix naming

--- Kysect.GithubActivityAnalyzer/DLL/Repositories/ActivityContext.cs
+++ Kysect.GithubActivityAnalyzer/DLL/Repositories/ActivityContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Kysect.GithubActivityAnalyzer.DLL.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -8,14 +9,18 @@ namespace Kysect.GithubActivityAnalyzer.DLL.Repositories
 {
     public class ActivityContext : DbContext
     {
-        public DbSet<UserCash> UserCash { get; set; }
+        public DbSet<UserСache> UserСache { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=D:\\LABS\\Projects\\ActivityParser\\myDB.db");
+            => options.UseSqlite($"Data Source=..\\myDB.db");
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<UserCash>()
+            modelBuilder.Entity<UserСache>()
                 .HasKey(b => b.Username)
                 .HasName("PrimaryKey_Username");
+        }
+        public ActivityContext()
+        {
+            Database.EnsureCreated();
         }
     }
 }
