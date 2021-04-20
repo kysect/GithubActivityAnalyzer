16510363e7e808770523fb74f30f022049d9b656
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-04-20 14:12:16 +0300

    add IRepository, StudentsGroupRepository

---
+++ Kysect.GithubActivityAnalyzer/Data/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Kysect.GithubActivityAnalyzer.Data.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        TEntity Create(TEntity item);
+        IQueryable<TEntity> Get();
+        IQueryable<TEntity> GetAll();
+        void Update(TEntity item);
+        void Delete(TEntity item);
+    }
+}
