c8326affdb25e209013c8e6fddf6463f70dd59f7
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-04-26 21:14:40 +0300

    feat: add ActivityService

---
+++ Kysect.GithubActivityAnalyzer.WebDemo/Server/Services/ActivityService.cs
@@ -0,0 +1,28 @@
+﻿using Kysect.GithubActivityAnalyzer.ApiAccessor;
+using Kysect.GithubActivityAnalyzer.ApiAccessor.ApiResponses;
+using Kysect.GithubActivityAnalyzer.Data.Repositories;
+using System.Threading.Tasks;
+
+namespace Kysect.GithubActivityAnalyzer.WebDemo.Server.Services
+{
+    public class ActivityService : IActivityService
+    {
+        private readonly GithubActivityProvider _githubActivityProvider;
+        private readonly UserCacheRepository _userСacheRepository;
+        public ActivityService(UserCacheRepository userСacheRepository, GithubActivityProvider githubActivityProvider)
+        {
+            this._userСacheRepository = userСacheRepository;
+            this._githubActivityProvider = githubActivityProvider;
+        }
+
+        public async Task<ActivityInfo> GetActivityInfoFromGithub(string username)
+        {
+            return await _githubActivityProvider.GetActivityInfo(username);
+        }
+
+        public ActivityInfo GetActivityInfoFromDB(string username)
+        {
+            return _userСacheRepository.GetActivityFromUserCash(_userСacheRepository.FindByUsername(username));
+        }
+    }
+}
