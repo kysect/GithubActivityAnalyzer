075de89fa2fcc02eb66c524aaf47436c97af9b81
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-05-05 19:02:02 +0300

    feat: add dbTeamController, TeamService

---
+++ Kysect.GithubActivityAnalyzer.WebDemo/Server/Controllers/DBTeamController.cs
@@ -0,0 +1,50 @@
+﻿using Kysect.GithubActivityAnalyzer.WebDemo.Server.Services;
+using Kysect.GithubActivityAnalyzer.WebDemo.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kysect.GithubActivityAnalyzer.WebDemo.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DBTeamController : Controller
+    {
+        private readonly ITeamService _teamService;
+        public DBTeamController(ITeamService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        [HttpPost("addTeam")]
+        public void AddTeam(Team newTeam)
+        {
+            _teamService.AddTeam(newTeam);
+        }
+
+        [HttpPost("deleteTeam")]
+        public void DeleteTeam(string teamName)
+        {
+            _teamService.DeleteTeam(teamName);
+        }  
+
+        [HttpPost("deleteMember")]
+        public void DeleteMember(string username)
+        {
+            _teamService.DeleteMember(username);
+        }
+
+        [HttpPost("updateMember")]
+        public void UpdateMember()
+        {
+        }
+
+        [HttpGet("GetAllTeams")]
+        public void GetAllTeams()
+        {
+        }
+
+        [HttpGet("GetTeam")]
+        public void GetTeam()
+        {
+        }
+    }
+}
