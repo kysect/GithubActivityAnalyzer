ce9b6549a585c7a481f45e2194b8e80f4d70370e
Fredi Kats <fredikats@outlook.com>
2021-05-20 20:41:23 +0300

    feat: add reading data from file

--- Kysect.GithubActivityAnalyzer.Sample/Program.cs
+++ Kysect.GithubActivityAnalyzer.Sample/Program.cs
@@ -1,10 +1,40 @@
 ﻿
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Kysect.GithubActivityAnalyzer.Aggregators;
+using Kysect.GithubActivityAnalyzer.Aggregators.Models;
+using Kysect.GithubActivityAnalyzer.ApiAccessor;
+using Kysect.GithubActivityAnalyzer.Services;
+
 namespace Kysect.GithubActivityAnalyzer.Sample
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var githubActivityProvider = new GithubActivityProvider();
+            List<UserWithTag> grouped = ReadFromFile();
+
+            List<Team> studyGroups = Team.CreateFromUserList(grouped, githubActivityProvider);
+
+            var excelExportManager = new ExcelExportManager(studyGroups);
+            excelExportManager.ExportFullInfo();
+            excelExportManager.SaveExcel("data.xlsx");
+        }
+
+        private static List<UserWithTag> ReadFromFile()
+        {
+            List<UserWithTag> grouped = File
+                .ReadLines("Data.txt")
+                .Select(s =>
+                {
+                    string[] strings = s.Split("\t");
+                    return new UserWithTag(strings[1], strings[0]);
+                })
+                .ToList();
+
+            return grouped;
         }
     }
 }
