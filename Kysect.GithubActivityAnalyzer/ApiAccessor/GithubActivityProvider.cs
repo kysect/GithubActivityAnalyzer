561325d847f6eb6c2aba750d850a8d6aa48e4663
Fredi Kats <fredikats@outlook.com>
2021-05-20 20:29:35 +0300

    feat: add retries for data fetching

--- Kysect.GithubActivityAnalyzer/ApiAccessor/GithubActivityProvider.cs
+++ Kysect.GithubActivityAnalyzer/ApiAccessor/GithubActivityProvider.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Kysect.GithubActivityAnalyzer.ApiAccessor.ApiResponses;
 
@@ -27,7 +29,7 @@ namespace Kysect.GithubActivityAnalyzer.ApiAccessor
             return activityInfo.FilterValues(from, to);
         }
 
-        public List<(string Username, ActivityInfo Activity)> GetActivityInfo(IEnumerable<string> usernames, bool isParallel, DateTime? from = null, DateTime? to = null)
+        public List<(string Username, ActivityInfo Activity)> GetActivityInfo(IReadOnlyCollection<string> usernames, bool isParallel, DateTime? from = null, DateTime? to = null)
         {
             if (!isParallel)
             {
@@ -36,10 +38,45 @@ namespace Kysect.GithubActivityAnalyzer.ApiAccessor
                     .ToList();
             }
 
-            List<(string, ActivityInfo)> result = usernames
+            return usernames
                 .AsParallel()
                 .Select(username => (username, GetActivityInfo(username, @from, to).Result))
                 .ToList();
+        }
+
+        /// <summary>
+        /// Sometimes result is empty. We will try to resend request to ensure that we cannot fetch data.
+        /// </summary>
+        /// <param name="usernames"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public List<(string Username, ActivityInfo Activity)> GetInfoWithRetry(IReadOnlyCollection<string> usernames, DateTime? from = null, DateTime? to = null)
+        {
+            int tryCount = 5;
+
+            List<(string Username, ActivityInfo Activity)> result = new List<(string Username, ActivityInfo Activity)>();
+            HashSet<string> processedUsers = new HashSet<string>();
+
+            Debug.Print($"Start processing: {usernames.Count}");
+            for (int i = 0; i < tryCount && result.Count != usernames.Count; i++)
+            {
+                if (i != 0)
+                {
+                    Debug.Print($"Elements for processing left: {usernames.Count - result.Count}");
+                    Thread.Sleep(500);
+                }
+
+                List<(string Username, ActivityInfo Result)> localResult = usernames
+                    .Where(u => !processedUsers.Contains(u))
+                    .AsParallel()
+                    .Select(username => (username, GetActivityInfo(username, @from, to).Result))
+                    .Where(r => r.Result.Total > 0)
+                    .ToList();
+
+                result.AddRange(localResult);
+                localResult.ForEach(r => processedUsers.Add(r.Username));
+            }
 
             return result;
         }
