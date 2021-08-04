7dbc157d33fdf6fbff28db5b9ba36b2925525507
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-08-04 09:12:46 +0300

    feat: try to handle unknown teamNames

--- Kysect.GithubActivityAnalyzer.WebDemo/Client/Pages/Dashboards/TeamDashboard.razor.cs
+++ Kysect.GithubActivityAnalyzer.WebDemo/Client/Pages/Dashboards/TeamDashboard.razor.cs
@@ -4,6 +4,7 @@ using Kysect.GithubActivityAnalyzer.WebDemo.Shared;
 using Kysect.GithubActivityAnalyzer.Aggregators.Models;
 using System.Net.Http.Json;
 using Microsoft.JSInterop;
+using System.Linq;
 
 namespace Kysect.GithubActivityAnalyzer.WebDemo.Client.Pages.Dashboards
 {
@@ -11,21 +12,28 @@ namespace Kysect.GithubActivityAnalyzer.WebDemo.Client.Pages.Dashboards
     {
         private string _teamName = String.Empty;
         private TeamResponse _team;
-        private bool _statsIsVisible = false;
+        private bool _statsIsVisible = true;
         private async Task GetStat()
         {
-            var teamResponse = await Http.PostAsJsonAsync("DBTeam/GetTeam", new Team(_teamName));
-            Team teamInfo = await teamResponse.Content.ReadFromJsonAsync<Team>();
-            var response = await Http.PostAsJsonAsync("Team", teamInfo);
-            _team = await response.Content.ReadFromJsonAsync<TeamResponse>();
-            _statsIsVisible = true;
+            var team = await Http.PostAsJsonAsync("DBTeam/GetTeam", new Team(_teamName));
+            Team teamToAggregate = await team.Content.ReadFromJsonAsync<Team>();
+            if (teamToAggregate.Usernames.Any())
+            {
+                var teamResponse = await Http.PostAsJsonAsync("Team", teamToAggregate);
+                _team = await teamResponse.Content.ReadFromJsonAsync<TeamResponse>();
+                _statsIsVisible = true;
+            }
+            else { _statsIsVisible = false;}
         }
         private async void GenerateBarChart()
         {
             await GetStat();
-            var teamInfo = _team.Members;
-            await jsRuntime.InvokeVoidAsync("GenerateBarChart", teamInfo);
+            StateHasChanged();
+            if (_statsIsVisible)
+            {
+                var teamInfo = _team.Members;
+                await jsRuntime.InvokeVoidAsync("GenerateBarChart", teamInfo);
+            }
         }
-
     }
 }
